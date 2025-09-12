9673ee7e141c585f5460bcadfc02e0648239d0da
SajjaPremsai <sajjapremsai8938@gmail.com>
2025-09-12 15:50:28 +0530

    Added Search.py

---
+++ Search.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Search {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = in.nextInt();
+        
+        int ele = in.nextInt();
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] == ele){
+                System.out.println(i);
+                System.exit(0);
+            }
+        }
+        System.out.println("Not Found");
+    }
+}
